792494849f03b28366c587a83792fbec336af66e
anwar <anwarhussein2353@gmail.com>
2021-08-28 12:30:58 -0500

    Added Delete Button

--- client.js
+++ client.js
@@ -4,7 +4,37 @@ $(document).ready(readyNow);
 
 
 
-
 function readyNow() {
+$('#Submit-button').on('click',addEmployee);
+$('#tableEmployee').on('click','#delete-button',removeEmployee);
+
     
+}
+function addEmployee() {
+   
+
+    let firstName = $('#firstname').val();
+    let lastName = $('#lastname').val();
+    let id = $('#id').val();
+    let title = $('#title').val();
+    let annualSalary = $('#annualsalary').val();
+    $('#tableEmployee').append(`<tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${id}</td>
+    <td>${title}</td>
+    <td>${annualSalary}</td>
+    <td><button id= "delete-button">Delete</button></td>
+    </tr>`);
+    
+$('#firstname').val('');
+$('#lastname').val('');
+$('#id').val('');
+$('#title').val('');
+$('#annualsalary').val('');
+    
+}
+function removeEmployee(){
+    console.log($(this));
+    $(this).parent().parent().remove();
 }
